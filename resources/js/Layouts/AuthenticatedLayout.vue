346afe8e51bff9cd9f00fd465e53c2f9683d6341
EdsonMartinez <EdsonMaartinez@hotmail.com>
2024-09-12 14:01:05 -0600

    Logo redirecting

--- resources/js/Layouts/AuthenticatedLayout.vue
+++ resources/js/Layouts/AuthenticatedLayout.vue
@@ -21,7 +21,7 @@ const user = usePage().props.auth.user;
                         <div class="flex">
                             <!-- Logo -->
                             <div class="shrink-0 flex items-center">
-                                <Link :href="route('dashboard')">
+                                <Link :href="route('quizMatches.index')">
                                     <ApplicationLogo
                                         class="block h-12 -ml-11 fill-current text-gray-800 dark:text-gray-200"
                                     />
